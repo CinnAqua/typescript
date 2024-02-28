50b1b2c61be6fd0381e303019719ce7cd1f9df81
CinnAqua <miguelcp1008@gmail.com>
2024-02-28 17:06:07 -0300

    commit inicial

---
+++ src/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (numero1, numero2) => numero1 * numero2;
+console.log(multiplicacao(3, 4));
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(saudacao('Miguel'));
---
+++ src/exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (numero1: number, numero2: number): number => numero1 * numero2
+
+console.log(multiplicacao(3, 4))
+
+const saudacao = (nome: string) => `Olá ${nome}`
+
+console.log(saudacao('Miguel'))
